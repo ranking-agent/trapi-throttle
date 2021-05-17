ecc0602d9ba33b4945619644ecd72b54c5809f70
Alon Greyber <alon@greyber.org>
2021-05-17 15:38:34 -0400

    Added query graph and message validation functions to utils

--- tests/utils.py
+++ tests/utils.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager, AsyncExitStack
 from urllib.parse import urlparse
 from functools import partial, wraps
 import datetime
+import inspect
 
 from fastapi import FastAPI, Response
 from asgiar import ASGIAR
@@ -69,3 +70,161 @@ def with_context(context, *args_, **kwargs_):
     return decorator
 
 with_response_overlay = partial(with_context, response_overlay)
+
+
+def validate_message(template, value):
+    """
+    Validate a message against the given template
+
+    Raises ValueError if anything doesn't match up.
+
+    Templates must be a dictionary with "knowledge_graph" and "results"
+    keys. Knowledge_graph should be a string representation of the knowledge
+    graph edges. Results should be a list of strings each representing a results object.
+    """
+
+    template["knowledge_graph"] = inspect.cleandoc(template["knowledge_graph"])
+
+    nodes = set()
+    # Validate edges
+    for edge_string in template["knowledge_graph"].splitlines():
+        sub, predicate, obj = edge_string.split(" ")
+        nodes.add(sub)
+        nodes.add(obj)
+        # Check that this edge exists
+        if not any(
+                edge["subject"] == sub and
+                edge["object"] == obj and
+                predicate in edge["predicate"]
+            for edge in value["knowledge_graph"]["edges"].values()
+        ):
+            raise ValueError(
+                f"Knowledge graph edge {edge_string} not found in message")
+
+    # Validate nodes
+    for node in nodes:
+        if node not in value["knowledge_graph"]["nodes"].keys():
+            raise ValueError(
+                f"Knowledge graph node {node} not found in message")
+
+    # Check for extra nodes or edges
+    if len(nodes) != len(value["knowledge_graph"]["nodes"]):
+        raise ValueError(
+            "Extra nodes found in message knowledge_graph")
+    if (
+        len(template["knowledge_graph"].splitlines()) !=
+        len(value["knowledge_graph"]["edges"])
+    ):
+        raise ValueError(
+            "Extra edges found in message knowledge_graph")
+
+    # Validate results
+    for index, template_result_string in enumerate(template["results"]):
+
+        # Parse string representation
+        template_result_string = inspect.cleandoc(template_result_string)
+        template_result = {}
+        current_key = None
+        for line in template_result_string.splitlines():
+            if not line.startswith(" "):
+                # Key (remove trailing colon)
+                current_key = line[:-1]
+                template_result[current_key] = []
+            else:
+                # Value
+                template_result[current_key].append(line.strip())
+
+        try:
+            value_result = value["results"][index]
+        except IndexError as e:
+            raise ValueError("Expected more results") from e
+
+        # Validate node bindings
+        for node_binding_string in template_result["node_bindings"]:
+            qg_node_id, *kg_node_ids = node_binding_string.split(" ")
+            if qg_node_id not in value_result["node_bindings"]:
+                raise ValueError(
+                    f"Could not find binding for node {qg_node_id}")
+
+            for kg_node_id in kg_node_ids:
+                if not any(
+                    nb["id"] == kg_node_id
+                    for nb in value_result["node_bindings"][qg_node_id]
+                ):
+                    raise ValueError(
+                        f"Expected node binding {qg_node_id} to {kg_node_id}")
+            if len(value_result["node_bindings"][qg_node_id]) != len(kg_node_ids):
+                raise ValueError(f"Extra node bindings found for {qg_node_id}")
+
+        # Validate edge bindings
+        for edge_binding_string in template_result["edge_bindings"]:
+            qg_edge_id, *kg_edge_strings = edge_binding_string.split(" ")
+
+            # Find KG edge IDs from the kg edge strings
+            kg_edge_ids = []
+            for kg_edge_string in kg_edge_strings:
+                sub, obj = kg_edge_string.split("-")
+                kg_edge_id = next(
+                    kg_edge_id
+                    for kg_edge_id, kg_edge in value["knowledge_graph"]["edges"].items()
+                    if kg_edge["subject"] == sub and kg_edge["object"] == obj
+                )
+                kg_edge_ids.append(kg_edge_id)
+
+            if qg_edge_id not in value_result["edge_bindings"]:
+                raise ValueError(
+                    f"Could not find binding for edge {qg_edge_id}")
+
+            for kg_edge_id in kg_edge_ids:
+                if not any(
+                    nb["id"] == kg_edge_id
+                    for nb in value_result["edge_bindings"][qg_edge_id]
+                ):
+                    raise ValueError(
+                        f"Expected edge binding {qg_edge_id} to {kg_edge_id}")
+            if len(value_result["edge_bindings"][qg_edge_id]) != len(kg_edge_ids):
+                raise ValueError(f"Extra edge bindings found for {qg_edge_id}")
+
+    # Check for extra results
+    if len(template["results"]) != len(value["results"]):
+        raise ValueError("Extra results found")
+
+
+def query_graph_from_string(s):
+    """
+    Parse a query graph from Mermaid flowchart syntax.
+    Useful for writing examples in tests.
+
+    Syntax information can be found here:
+    https://mermaid-js.github.io/mermaid/#/flowchart
+
+    You can use this site to preview a query graph:
+    https://mermaid-js.github.io/mermaid-live-editor/
+    """
+
+    # This usually comes from triple quoted strings
+    # so we use inspect.cleandoc to remove leading indentation
+    s = inspect.cleandoc(s)
+
+    node_re = r"(?P<id>.*)\(\( (?P<key>.*) (?P<val>.*) \)\)"
+    edge_re = r"(?P<src>.*)-- (?P<predicate>.*) -->(?P<target>.*)"
+    qg = {"nodes": {}, "edges": {}}
+    for line in s.splitlines():
+        match_node = re.search(node_re, line)
+        match_edge = re.search(edge_re, line)
+        if match_node:
+            node_id = match_node.group('id')
+            if node_id not in qg['nodes']:
+                qg['nodes'][node_id] = {}
+            qg['nodes'][node_id][match_node.group('key')] = \
+                match_node.group('val')
+        elif match_edge:
+            edge_id = match_edge.group('src') + match_edge.group('target')
+            qg['edges'][edge_id] = {
+                "subject": match_edge.group('src'),
+                "object": match_edge.group('target'),
+                "predicate": match_edge.group('predicate'),
+            }
+        else:
+            raise ValueError(f"Invalid line: {line}")
+    return qg
