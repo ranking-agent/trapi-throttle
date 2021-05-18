61d3cadd45ef74d507f4f833eebda1be4eb9ccbc
Alon Greyber <alon@greyber.org>
2021-05-18 12:14:58 -0400

    Switched to middleware for rate limiting

--- tests/utils.py
+++ tests/utils.py
@@ -6,15 +6,50 @@ import datetime
 import inspect
 
 from fastapi import FastAPI, Response
+from starlette.middleware.base import BaseHTTPMiddleware
 from asgiar import ASGIAR
 
 def url_to_host(url):
     # TODO modify ASGIAR to accept a URL instead of a host
     return urlparse(url).netloc
 
-# Store rate limit information in a global
-# variable for simplicity
-rate_limit_tracker = {}
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    """
+    Middleware to enforce a rate limit
+
+    Returns 429 if the rate limit is exceeded
+    """
+    def __init__(self, app,
+                 request_qty: int,
+                 request_duration: datetime.timedelta
+                 ):
+        super().__init__(app)
+        self.request_qty = request_qty
+        self.request_duration = request_duration
+        self.rate_limit_tracker = {}
+
+    async def dispatch(self, request, call_next):
+        if "start_time" not in self.rate_limit_tracker:
+            self.rate_limit_tracker = {
+                "start_time" : datetime.datetime.now(),
+                "qty" : 0
+            }
+        time_since_last_request = \
+            datetime.datetime.now() - self.rate_limit_tracker["start_time"]
+
+        if time_since_last_request > self.request_duration:
+            # Reset rate limit
+            self.rate_limit_tracker["start_time"] = datetime.datetime.now()
+            self.rate_limit_tracker["qty"] = 0
+
+        # Check that we are not above quantity
+        self.rate_limit_tracker["qty"] += 1
+        if self.rate_limit_tracker["qty"] > self.request_qty:
+            return Response(status_code = 429)
+
+        response = await call_next(request)
+        return response
+
 
 @asynccontextmanager
 async def response_overlay(
@@ -34,25 +69,13 @@ async def response_overlay(
         # pylint: disable=unused-variable disable=unused-argument
         @app.api_route('/{path:path}', methods=["GET", "POST", "PUT", "DELETE"])
         async def all_paths(path):
-            if url not in rate_limit_tracker:
-                rate_limit_tracker[url] = {
-                    "start_time" : datetime.datetime.now(),
-                    "qty" : 0
-                }
-            time_since_last_request = \
-                datetime.datetime.now() - rate_limit_tracker[url]["start_time"]
-
-            if time_since_last_request > request_duration:
-                # Reset rate limit
-                rate_limit_tracker[url]["start_time"] = datetime.datetime.now()
-                rate_limit_tracker[url]["qty"] = 0
-
-            # Check that we are not above quantity
-            rate_limit_tracker[url]["qty"] += 1
-            if rate_limit_tracker[url]["qty"] > request_qty:
-                return Response(status_code = 429)
-
             return response
+
+        app.add_middleware(
+            RateLimitMiddleware,
+            request_qty = request_qty,
+            request_duration = request_duration,
+        )
 
         await stack.enter_async_context(
             ASGIAR(app, host=url_to_host(url))
