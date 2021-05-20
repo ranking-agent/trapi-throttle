7a678ec9debe17113e39f8d7c4a4dc69ac688e72
Alon Greyber <alon@greyber.org>
2021-05-20 09:51:16 -0400

    Added test to ensure KP rate limiting is applied

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ import httpx
 from starlette.responses import Response
 import pytest
 
-from .utils import with_response_overlay
+from .utils import with_response_overlay, with_kp_overlay
 
 @pytest.mark.asyncio
 @with_response_overlay(
@@ -14,7 +14,7 @@ from .utils import with_response_overlay
     request_qty = 2,
     request_duration = datetime.timedelta(seconds = 1)
 )
-async def test_rate_limiting():
+async def test_response_rate_limiting():
     """ Test that our utility processes rate limits correctly """
     async with httpx.AsyncClient() as client:
         response = await client.get("http://kp/query/1")
@@ -27,4 +27,48 @@ async def test_rate_limiting():
         await asyncio.sleep(1)
 
         response = await client.get("http://kp/query")
+        assert response.status_code == 200
+
+@pytest.mark.asyncio
+@with_kp_overlay(
+    "http://kp/query",
+    kp_data = \
+        """
+        MONDO:0005148(( category biolink:Disease ))
+        CHEBI:6801(( category biolink:ChemicalSubstance ))
+        CHEBI:6801-- predicate biolink:treats -->MONDO:0005148
+        """,
+    request_qty = 2,
+    request_duration = datetime.timedelta(seconds = 1)
+)
+async def test_kp_rate_limiting():
+    """ Test that our kp utility processes rate limits correctly """
+
+    qg = {
+        "nodes" : {
+            "n0" : { "ids" : ["CHEBI:6801"] },
+            "n1" : { "categories" : ["biolink:Disease"] },
+        },
+        "edges" : {
+            "n0n1" : {
+                "subject" : "n0",
+                "object" : "n1",
+                "predicates" : ["biolink:treats"],
+            }
+        },
+    }
+
+    query = {"message" : {"query_graph" : qg}}
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post("http://kp/query", json = query)
+        assert response.status_code == 200
+        response = await client.post("http://kp/query", json = query)
+        assert response.status_code == 200
+        response = await client.post("http://kp/query", json = query)
+        assert response.status_code == 429
+
+        await asyncio.sleep(1)
+
+        response = await client.post("http://kp/query", json = query)
         assert response.status_code == 200
