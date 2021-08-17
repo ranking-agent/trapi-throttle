069192ceede2403662ff4e9e85d659d03e27df22
Patrick Wang <patrickkwang@users.noreply.github.com>
2021-08-17 13:48:32 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="trapi-throttle",
-    version="1.2.0",
+    version="1.2.1",
     author="Patrick Wang",
     author_email="patrick@covar.com",
     url="https://github.com/ranking-agent/trapi-throttle",
